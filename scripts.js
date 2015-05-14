a58eb659a6dd4ab9fb8512ef62c7d6a699bdc753
Billy Farroll <watanight@hotmail.co.uk>
2015-05-14 23:42:07 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,41 @@
+var main = function() {
+    
+    $('.btn').click(function() {
+    
+    var post = $('.status-box').val(); 
+      $('<li>').text(post).prependTo('.posts');
+      $('.status-box').val('');
+      $('.counter').text('140'); 
+      $('.btn').addClass('disabled');
+});
+
+    $('.status-box').keyup(function() { 
+    
+    var postlength = $(this).val().length; 
+    var charactersLeft = 140 - postlength;
+    $('.counter').text(charactersLeft); 
+
+
+    if (charactersLeft < 0) { 
+     ('.btn').addClass('disabled'); 
+        
+}
+    else if(charactersLeft == 140){
+     $('.btn').addClass('disabled');   
+        
+    }
+    
+    else {
+        
+        ('.btn').removeClass('disabled');
+      }
+    
+    });
+
+ 
+}
+
+ $('.btn').addClass('disabled');
+
+
+$(document).ready(main);
